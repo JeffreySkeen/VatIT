60af0e2f046b246bd5e77c8ce426205802052cec
Jeffrey <skeenjeffrey@gmail.com>
2021-09-27 12:26:16 +0200

    Moved the ToDo object out of Models folder, Created the Todos result model and Created the ToDo result

--- VatIT/Controllers/ToDoController.cs
+++ VatIT/Controllers/ToDoController.cs
@@ -4,84 +4,16 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using VatIT.Models;
 
 namespace VatIT.Controllers
 {
     public class ToDoController : Controller
     {
-        // GET: ToDoController
-        public ActionResult Index()
+        private IList<ToDo> ToDos;
+        public ToDoController()
         {
-            return View();
-        }
-
-        // GET: ToDoController/Details/5
-        public ActionResult Details(int id)
-        {
-            return View();
-        }
-
-        // GET: ToDoController/Create
-        public ActionResult Create()
-        {
-            return View();
-        }
-
-        // POST: ToDoController/Create
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Create(IFormCollection collection)
-        {
-            try
-            {
-                return RedirectToAction(nameof(Index));
-            }
-            catch
-            {
-                return View();
-            }
-        }
-
-        // GET: ToDoController/Edit/5
-        public ActionResult Edit(int id)
-        {
-            return View();
-        }
-
-        // POST: ToDoController/Edit/5
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Edit(int id, IFormCollection collection)
-        {
-            try
-            {
-                return RedirectToAction(nameof(Index));
-            }
-            catch
-            {
-                return View();
-            }
-        }
-
-        // GET: ToDoController/Delete/5
-        public ActionResult Delete(int id)
-        {
-            return View();
-        }
-
-        // POST: ToDoController/Delete/5
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Delete(int id, IFormCollection collection)
-        {
-            try
-            {
-                return RedirectToAction(nameof(Index));
-            }
-            catch
-            {
-                return View();
-            }
+            this.ToDos = new List<ToDo>() { new ToDo("Todo1", 1, false), new ToDo("Todo2", 2, false), new ToDo("Todo3", 3, false), new ToDo("Todo4", 4, false) };
         }
     }
 }
---
+++ VatIT/Results/GetTodosResult.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Threading.Tasks;
+
+namespace VatIT.Results
+{
+    [DataContract]
+    public class GetTodosResult
+    {
+        [DataMember]
+        public IList<ToDo> ToDos { get; set; }
+    }
+}
