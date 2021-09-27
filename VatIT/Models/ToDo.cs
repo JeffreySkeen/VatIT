6dcd5bac0f25a5fcd82a82f86e54a9ad9b46af34
Jeffrey <skeenjeffrey@gmail.com>
2021-09-27 12:04:47 +0200

    Created ToDo object

---
+++ VatIT/Models/ToDo.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace VatIT.Models
+{
+    public class ToDo
+    {
+        private readonly int ID;
+        private string name;
+        private bool completed;
+        public ToDo(string name, int ID, bool completed)
+        {
+            this.ID = ID;
+            this.name = name;
+            this.completed = completed;
+        }
+
+        public static ToDo Create(string name, int ID, bool completed)
+        {
+            return new ToDo(name,ID,completed);
+        }
+
+        public bool Completed
+        {
+            get
+            {
+                return this.completed;
+            }
+            set
+            {
+                this.completed = value;
+            }
+        }
+
+        public string Name
+        {
+            get
+            {
+                return this.Name;
+            }
+            set
+            {
+                this.name = value
+            }
+        }
+}
